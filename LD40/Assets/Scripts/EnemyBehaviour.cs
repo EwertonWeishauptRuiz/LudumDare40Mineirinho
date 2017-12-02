c45d1ca7c02dbc3b91d7319ab8d5f27b6ea9f742
Ewerton <shakamaiden@gmail.com>
2017-12-03 00:02:07 +0100

    Enemy follow very basic

---
+++ LD40/Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+    public Transform target;
+
+    int moveSpeed = 2;
+    float minDistance = .5f;
+    int maxDistance = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(target);
+
+        if(Vector3.Distance(transform.position, target.position) >= minDistance) {
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;            
+        }
+        if (Vector3.Distance(transform.position, target.position) > maxDistance) {
+            transform.position += transform.forward * (moveSpeed + 5) * Time.deltaTime;            
+        }
+    }
+}
