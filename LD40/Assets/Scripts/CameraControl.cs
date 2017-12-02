84de39e5144bf1c01b27b9a7b40923deeaa80971
Rafael Moreira <me@rafaelmoreira.net>
2017-12-02 11:02:07 -0200

    add camera follow

---
+++ LD40/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CameraControl : MonoBehaviour
+{
+
+    public Transform target;
+    public Vector3 offset;
+    public float smoothTime = 0.3f;
+    Vector3 velocity = Vector3.zero;
+    
+
+    private void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+
+        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothTime);
+    }
+}
