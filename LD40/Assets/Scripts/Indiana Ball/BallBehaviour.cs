e0f0a532d7122c1b810e86739f081b27aa6739cd
Ewerton <shakamaiden@gmail.com>
2017-12-02 23:36:06 +0100

    Indiana Ball Roll

---
+++ LD40/Assets/Scripts/Indiana Ball/BallBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehaviour : MonoBehaviour {
+
+    public float lifeTime = 5f;
+    
+	// Update is called once per frame
+	void Update () {
+        if(lifeTime <= 0) {
+            Destroy(gameObject);
+        }		
+        lifeTime -= Time.deltaTime;        
+	}
+}
---
+++ LD40/Assets/Scripts/Indiana Ball/IndianaBallController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndianaBallController : MonoBehaviour {
+
+    public Transform spawnLocation;
+    public GameObject ballPrefab;
+    
+    // Use this for initialization
+    void Start() {
+        StartCoroutine("SpawnBall");
+    }
+    
+    void Instantiateball() {
+        GameObject ball = Instantiate(ballPrefab, spawnLocation.transform.position, Quaternion.Euler(0, 0, 90));
+        Rigidbody rbd = ball.GetComponent<Rigidbody>();
+        rbd.velocity = new Vector3(0, 0, -20);
+    }
+
+    public IEnumerator SpawnBall() {
+        while (true) {
+            Instantiateball();            
+            yield return new WaitForSeconds(2f);
+        }
+    }
+}
