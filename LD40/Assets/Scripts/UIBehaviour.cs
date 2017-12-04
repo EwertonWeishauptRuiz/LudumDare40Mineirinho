583ef0af5fc317d62c50b1401d08688d0f4cea29
Rubito Lisa <rubens.cesar.junior@hotmail.com>
2017-12-04 16:56:58 -0500

    UI:Gold+Silver

--- LD40/Assets/Scripts/UIBehaviour.cs
+++ LD40/Assets/Scripts/UIBehaviour.cs
@@ -2,16 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class UIBehaviour : MonoBehaviour {
+public class UIBehaviour : MonoBehaviour
+{
+    #region HP
     PlayerHealth healthScript;
     Text hpLeft;
+    #endregion
+    #region Items
+    ItemManager itemScript;
+    Text gold;
+    Text silver;
+    #endregion
+    void Start()
+    {
+#region HP
+        hpLeft = GetComponent<Text>();
+        healthScript = GameObject.FindObjectOfType<PlayerHealth>();
 
- void Start () {
-    hpLeft = GetComponent<Text>();
-    healthScript = GameObject.FindObjectOfType<PlayerHealth>();
+        #endregion
+#region Items
+        itemScript = GameObject.FindObjectOfType<ItemManager>();
+        gold = GameObject.FindGameObjectWithTag("Gold").GetComponent<Text>();
+        silver = GameObject.FindGameObjectWithTag("Silver").GetComponent<Text>();
+        #endregion
     }
-    void Update()    {
-        
+    void Update()
+    {
+#region HP 
         hpLeft.text = "x" + healthScript.health;
+
+        #endregion
+#region Items
+        silver.text = "x" + itemScript.carryingSilver;
+        gold.text = "x" + itemScript.carryingGold;
+        #endregion
     }
 }
