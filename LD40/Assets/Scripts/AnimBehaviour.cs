989b8995777ac421bbed0bb8e6f69b60bf247fad
Rubito Lisa <rubens.cesar.junior@hotmail.com>
2017-12-04 17:49:38 -0500

    AnimScript

---
+++ LD40/Assets/Scripts/AnimBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimBehaviour : MonoBehaviour {
+	Animator playerAnim;
+	PlayerController walkScript;
+	PlayerHealth damageScript;
+	float currentWalkingSpeed;
+
+	void Start () {
+		playerAnim = GetComponent<Animator> ();
+		walkScript = FindObjectOfType<PlayerController> ();
+		damageScript = FindObjectOfType<PlayerHealth> ();
+	}
+	void Update () {
+		currentWalkingSpeed = walkScript.walkSpeed;
+		playerAnim.SetFloat ("CurrentWalkingSpeed", currentWalkingSpeed);
+
+
+	}
+	public void Hurt(){
+		playerAnim.SetTrigger ("IsHurt");
+	}
+}
