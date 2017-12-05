a339cb893ab664a7667ab22759e48403ed7e0faa
Rubito Lisa <rubens.cesar.junior@hotmail.com>
2017-12-04 19:03:42 -0500

    Title

---
+++ LD40/Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour {
+
+	public void NewGame(){
+		SceneManager.LoadScene ("Animation Test", LoadSceneMode.Single);
+	}
+	public void Exit(){
+		Application.Quit ();
+	}
+}
